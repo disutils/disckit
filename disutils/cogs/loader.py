f9de0363b62d508fffc48cb98bda3d29c8df5652
Jiggly Balls <jigglyballs9000@gmail.com>
2024-11-01 12:17:33 +0530

    Finished cog loader

---
+++ disutils/cogs/loader.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from typing import Optional
+
+from disutils import CogEnum
+
+
+async def dis_load_extension(
+    bot: commands.Bot,
+    *cogs: CogEnum,
+    debug_message: Optional[str] = "Loading extension: {}",
+) -> None:
+    for cog in cogs:
+        await bot.load_extension(cog.value)
+        if debug_message:
+            print(
+                debug_message.format(
+                    cog.name.title().replace(" ", "")
+                )
+            )
