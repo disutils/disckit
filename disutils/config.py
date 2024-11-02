4caece37daadc64cc0fa072ca500ae241e37afc8
Jiggly Balls <jigglyballs9000@gmail.com>
2024-11-02 14:03:34 +0530

    Finished documenting & added full support to cogs

--- disutils/config.py
+++ disutils/config.py
@@ -1,9 +1,21 @@
 from __future__ import annotations
 
 import inspect
-from discord import Colour
-from typing import Optional, TypeAlias, Union, ClassVar, Type, Any
+from discord import Colour, ActivityType
+from typing import (
+    Optional,
+    TypeAlias,
+    Union,
+    ClassVar,
+    Type,
+    Awaitable,
+    Tuple,
+    List,
+    Set,
+    Any,
+)
 from enum import StrEnum
+from utils import default_status_handler
 
 TypeColor: TypeAlias = Union[int, Colour]
 _BASE_COG_PATH: str = "disutils/cogs/"
@@ -54,13 +66,21 @@ class _MetaClassProperty(type):
 
 
 class UtilConfig(metaclass=_MetaClassProperty):
+    """The utility class which configures disutils utilities."""
+
     _MAIN_COLOR: ClassVar[Optional[TypeColor]] = None
     _SUCCESS_COLOR: ClassVar[Optional[TypeColor]] = None
     _ERROR_COLOR: ClassVar[Optional[TypeColor]] = None
+    _SUCCESS_EMOJI: ClassVar[Optional[str]] = None
+    _ERROR_EMOJI: ClassVar[Optional[str]] = None
+    _FOOTER_IMAGE: ClassVar[Optional[str]] = None
     _FOOTER_TEXT: ClassVar[Optional[str]] = None
-    _GREEN_CHECK: ClassVar[Optional[str]] = None
-    _AVATAR_URL: ClassVar[Optional[str]] = None
-    _VERSION: ClassVar[Optional[str]] = None
+    _STATUS_FUNC: ClassVar[Tuple[Awaitable[Union[Tuple, List, Set]], Tuple]] = (
+        default_status_handler,
+        (),
+    )
+    _STATUS_TYPE: ClassVar[ActivityType] = ActivityType.listening
+    _STATUS_COOLDOWN: ClassVar[Optional[int]] = None
     _BUG_REPORT_CHANNEL: ClassVar[Optional[int]] = None
 
     @staticmethod
@@ -72,68 +92,183 @@ class UtilConfig(metaclass=_MetaClassProperty):
 
     @_classproperty
     def MAIN_COLOR(cls: Type[UtilConfig]) -> Optional[TypeColor]:
+        """Class Attribute
+        ---
+        MAIN_COLOR: :class:`Optional[int, discord.color.Color]`
+
+            The color of the MainEmbed.
+        """
         return cls._MAIN_COLOR
 
     @MAIN_COLOR.setter
     def MAIN_COLOR(cls: Type[UtilConfig], value: Optional[TypeColor]) -> None:
-        UtilConfig.__validator(value, Optional[TypeColor])
+        UtilConfig.__validator(value, TypeColor)
         cls._MAIN_COLOR = value
 
     @_classproperty
     def SUCCESS_COLOR(cls: Type[UtilConfig]) -> Optional[TypeColor]:
+        """Class Attribute
+        ---
+        SUCCESS_COLOR: :class:`Optional[int, discord.color.Color]`
+
+            The color of the SuccessEmbed.
+        """
         return cls._SUCCESS_COLOR
 
     @SUCCESS_COLOR.setter
     def SUCCESS_COLOR(cls: Type[UtilConfig], value: Optional[TypeColor]) -> None:
-        UtilConfig.__validator(value, Optional[TypeColor])
+        UtilConfig.__validator(value, TypeColor)
         cls._MAIN_COLOR = value
 
     @_classproperty
     def ERROR_COLOR(cls: Type[UtilConfig]) -> Optional[TypeColor]:
+        """Class Attribute
+        ---
+        ERROR_COLOR: :class:`Optional[int, discord.color.Color]`
+
+            The color of the ErrorEmbed.
+        """
         return cls._ERROR_COLOR
 
     @ERROR_COLOR.setter
     def ERROR_COLOR(cls: Type[UtilConfig], value: Optional[TypeColor]) -> None:
-        UtilConfig.__validator(value, Optional[TypeColor])
+        UtilConfig.__validator(value, TypeColor)
         cls._ERROR_COLOR = value
 
     @_classproperty
-    def GREEN_CHECK(cls: Type[UtilConfig]) -> Optional[str]:
-        return cls._GREEN_CHECK
+    def SUCCESS_EMOJI(cls: Type[UtilConfig]) -> Optional[str]:
+        """Class Attribute
+        ---
+        SUCCESS_EMOJI: :class:`Optional[str]`
 
-    @GREEN_CHECK.setter
-    def GREEN_CHECK(cls: Type[UtilConfig], value: Optional[str]) -> None:
-        UtilConfig.__validator(value, Optional[str])
-        cls._GREEN_CHECK = value
+            An emoji used in the title of the SuccessEmbed.
+        """
+        return cls._SUCCESS_EMOJI
 
-    @_classproperty
-    def AVATAR_URL(cls: Type[UtilConfig]) -> Optional[str]:
-        return cls._AVATAR_URL
-
-    @AVATAR_URL.setter
-    def AVATAR_URL(cls: Type[UtilConfig], value: Optional[str]) -> None:
-        UtilConfig.__validator(value, Optional[str])
-        cls._AVATAR_URL = value
+    @SUCCESS_EMOJI.setter
+    def SUCCESS_EMOJI(cls: Type[UtilConfig], value: Optional[str]) -> None:
+        UtilConfig.__validator(value, str)
+        cls._SUCCESS_EMOJI = value
 
     @_classproperty
-    def VERSION(cls: Type[UtilConfig]) -> Optional[str]:
-        return cls._VERSION
+    def ERROR_EMOJI(cls: Type[UtilConfig]) -> Optional[str]:
+        """Class Attribute
+        ---
+        ERROR_EMOJI: :class:`Optional[str]`
 
-    @VERSION.setter
-    def VERSION(cls: Type[UtilConfig], value: Optional[str]) -> None:
+            An emoji used in the title of the ErrorEmbed.
+        """
+        return cls._ERROR_EMOJI
+
+    @ERROR_EMOJI.setter
+    def ERROR_EMOJI(cls: Type[UtilConfig], value: Optional[str]) -> None:
+        UtilConfig.__validator(value, str)
+        cls._ERROR_EMOJI = value
+
+    @_classproperty
+    def FOOTER_IMAGE(cls: Type[UtilConfig]) -> Optional[str]:
+        """Class Attribute
+        ---
+        FOOTER_IMAGE: :class:`Optional[str]`
+
+            A URL to an image for the footer of `MainEmbed`, `SuccessEmbed` and `ErrorEmbed`.
+        """
+        return cls._FOOTER_IMAGE
+
+    @FOOTER_IMAGE.setter
+    def FOOTER_IMAGE(cls: Type[UtilConfig], value: Optional[str]) -> None:
+        UtilConfig.__validator(value, str)
+        cls._FOOTER_IMAGE = value
+
+    @_classproperty
+    def FOOTER_TEXT(cls: Type[UtilConfig]) -> Optional[TypeColor]:
+        """Class Attribute
+        ---
+        FOOTER_TEXT: :class:`Optional[str]`
+
+            The footer text of `MainEmbed`, `SuccessEmbed` and `ErrorEmbed`.
+        """
+        return cls._FOOTER_TEXT
+
+    @FOOTER_TEXT.setter
+    def FOOTER_TEXT(cls: Type[UtilConfig], value: Optional[str]) -> None:
         UtilConfig.__validator(value, Optional[str])
-        cls._VERSION = value
+        cls._FOOTER_TEXT = value
+
+    @_classproperty
+    def STATUS_FUNC(
+        cls: Type[UtilConfig],
+    ) -> Tuple[Awaitable[Union[Tuple, List, Set]], Tuple]:
+        """Class Attribute
+        ---
+        STATUS_FUNC: :class:`Tuple[Awaitable[Union[Tuple, List, Set]], Tuple]`
+
+            A tuple having it's first element as a coroutine object which will
+            be awaited when-<br>
+            - When the cog first loads.<br>
+            - When the handler is done iterating through all statuses returned from the function.<br>
+            The second element is a tuple containing the extra arguments that can be passed to your
+            custom status handler function. If no arguments has to be passed an empty tuple
+            should suffice.
+        """
+        return cls._STATUS_FUNC
+
+    @STATUS_FUNC.setter
+    def STATUS_FUNC(
+        cls: Type[UtilConfig], value: Tuple[Awaitable[Union[Tuple, List, Set]], Tuple]
+    ) -> None:
+        UtilConfig.__validator(value, Tuple[Awaitable[Union[Tuple, List, Set]], Tuple])
+        cls._STATUS_FUNC = value
+
+    @_classproperty
+    def STATUS_TYPE(cls: Type[UtilConfig]) -> ActivityType:
+        """Class Attribute
+        ---
+        STATUS_TYPE: :class:`ActivityType`
+
+            The discord acitvity type used by the StatusHandler.
+        """
+        return cls._STATUS_TYPE
+
+    @STATUS_TYPE.setter
+    def STATUS_TYPE(cls: Type[UtilConfig], value: ActivityType) -> None:
+        UtilConfig.__validator(value, ActivityType)
+        cls._STATUS_TYPE = value
+
+    @_classproperty
+    def STATUS_COOLDOWN(cls: Type[UtilConfig]) -> Optional[int]:
+        """Class Attribute
+        ---
+        STATUS_COOLDOWN: :class:`Optional[int]`
+
+            A cooldown for how long a status will play before changing in the `StatusHandler` cog.
+        """
+        return cls._STATUS_COOLDOWN
+
+    @STATUS_COOLDOWN.setter
+    def STATUS_COOLDOWN(cls: Type[UtilConfig], value: Optional[int]) -> None:
+        UtilConfig.__validator(value, int)
+        cls._STATUS_COOLDOWN = value
 
     @_classproperty
     def BUG_REPORT_CHANNEL(cls: Type[UtilConfig]) -> Optional[int]:
+        """Class Attribute
+        ---
+        BUG_REPORT_CHANNEL: :class:`Optional[int]`
+
+            The channel ID to where the bug reports will be sent to by the `ErrorHandler` cog.
+        """
         return cls.BUG_REPORT_CHANNEL
 
     @BUG_REPORT_CHANNEL.setter
     def BUG_REPORT_CHANNEL(cls: Type[UtilConfig], value: Optional[int]) -> None:
-        UtilConfig.__validator(value, Optional[int])
+        UtilConfig.__validator(value, int)
         cls._BUG_REPORT_CHANNEL = value
 
 
 class CogEnum(StrEnum):
     ERROR_HANDLER: str = _BASE_COG_PATH + "worker/error_handler"
+    """An extension for error handling."""
+
     STATUS_HANDLER: str = _BASE_COG_PATH + "worker/status_handler"
+    """An extension for the bot's status handling."""
