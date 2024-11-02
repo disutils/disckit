387f52144d6a01fb85ce69cc5c399176463da56c
Jiggly Balls <jigglyballs9000@gmail.com>
2024-11-02 14:04:39 +0530

    Finished cog loader & its documenting

--- disutils/cogs/loader.py
+++ disutils/cogs/loader.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 from typing import Optional
 
-from disutils import CogEnum
+from disutils.config import CogEnum, UtilConfig
+from disutils.errors import CogLoadError
 
 
 async def dis_load_extension(
@@ -9,7 +10,42 @@ async def dis_load_extension(
     *cogs: CogEnum,
     debug_message: Optional[str] = "Loading extension: {}",
 ) -> None:
+    """|coro|
+    A custom extension loader specifically for the disutil-dpy cogs.
+
+    Parameters
+    ----------
+    bot: :class:`commands.Bot`
+        The bot instance.
+        
+    *cogs: :class:`CogEnum`
+        The cogs to be loaded from disutil-dpy package.
+
+    debug_message: :class:`Optional[str]`
+        The debug message to be printed out when the cog is loaded.
+        
+    Raises
+    ------
+    :class:`CogLoadError`
+        Raised when an error occurrs in loading the cog.
+    """
+
+    message = None
     for cog in cogs:
+        if cog == CogEnum.STATUS_HANDLER:
+            if UtilConfig.STATUS_COOLDOWN is None:
+                message = f"Attribute: `UtilConfig.STATUS_COOLDOWN` needs"
+                " to be set before runtime to use StatusHandler cog."
+
+        if cog == CogEnum.ERROR_HANDLER:
+            if UtilConfig.BUG_REPORT_CHANNEL is None:
+                message = f"Attribute: `UtilConfig.BUG_REPORT_CHANNEL` needs"
+                " to be set before runtime to use ErrorHandler cog."
+
+        if message:
+            raise CogLoadError(message=message, cog=cog)
+    
+
         await bot.load_extension(cog.value)
         if debug_message:
             print(debug_message.format(cog.name.title().replace(" ", "")))
