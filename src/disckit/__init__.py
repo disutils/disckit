ce02c621ed1d8e0e98884079d25fca48363fe622
Jiggly Balls <jigglyballs9000@gmail.com>
2024-11-16 23:29:04 +0530

    Added project descriptions

--- src/disckit/__init__.py
+++ src/disckit/__init__.py
@@ -1,4 +1,19 @@
+"""
+Disutils' utility package
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A utility package made for the disutils bots.
+
+:copyright: (c) 2024-present Disutils Team
+:license: MIT, see LICENSE for more details.
+
+"""
+
 __version__ = "0.3"
+__title__ = "disckit"
+__author__ = "Jiggly Balls"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024-present Disutils Team"
 
 from disckit.config import UtilConfig, CogEnum
 from disckit.errors import DisException, CogLoadError
