a62e6dcc6da4da6f2479fabd50d6d3d81e3f4bb6
Jiggly Balls <jigglyballs9000@gmail.com>
2024-11-24 12:50:43 +0530

    Added version references

--- src/disckit/__init__.py
+++ src/disckit/__init__.py
@@ -9,7 +9,7 @@ A utility package made for the disutils bots.
 
 """
 
-__version__ = "0.3"
+__version__ = "0.4b"
 __title__ = "disckit"
 __author__ = "Jiggly Balls"
 __license__ = "MIT"
@@ -17,3 +17,20 @@ __copyright__ = "Copyright 2024-present Disutils Team"
 
 from disckit.config import UtilConfig, CogEnum
 from disckit.errors import DisException, CogLoadError
+from typing import NamedTuple, Literal
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    release_level: Literal["alpha", "beta", "final"]
+
+
+def _expand() -> VersionInfo:
+    v = __version__.split(".")
+    level_types = {"a": "alpha", "b": "beta"}
+    level = level_types.get(v[-1], "final")
+    return VersionInfo(major=v[0], minor=v[1], release_level=level)
+
+
+version_info: VersionInfo = _expand()
