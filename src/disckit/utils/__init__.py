e173b5838cf80ec2b5a9f334e193bfd993f9632f
Jiggly Balls <jigglyballs9000@gmail.com>
2025-01-27 18:13:07 +0530

    Fixed typehint

--- src/disckit/utils/__init__.py
+++ src/disckit/utils/__init__.py
@@ -1,24 +1,27 @@
-from typing import Tuple, List, Any, Callable, Awaitable, Union
-from discord import Interaction
-from discord.ext import commands
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 from discord.app_commands import Choice
 from disckit.utils.embeds import SuccessEmbed, MainEmbed, ErrorEmbed
 
-# from disckit.utils.translator import LemmaTranslator
+if TYPE_CHECKING:
+    from discord import Interaction
+    from discord.ext.commands import Bot
+    from typing import Any, Awaitable, Callable, List, Tuple, TypeVar
+
+    _T = TypeVar("T", str, int, float)
+
 
 __all__ = (
     "MainEmbed",
     "SuccessEmbed",
     "ErrorEmbed",
-    #    "LemmaTranslator",
     "default_status_handler",
     "make_autocomplete",
 )
 
 
-async def default_status_handler(
-    bot: commands.Bot, *args: Any
-) -> Tuple[str, ...]:
+async def default_status_handler(bot: Bot, *args: Any) -> Tuple[str, ...]:
     """The default status handler. The first parameter will always be the
     bot instance which will automatically be passed as argument in the
     status handler.
@@ -56,8 +59,8 @@ async def default_status_handler(
 
 
 def make_autocomplete(
-    *args: Union[str, int, float],
-) -> Callable[[Interaction, str], Awaitable[list[Choice[str]]]]:
+    *args: _T,
+) -> Callable[[Interaction, str], Awaitable[list[Choice[_T]]]]:
     """
     Creates an autocomplete function for the given arguments.
 
@@ -81,7 +84,7 @@ def make_autocomplete(
     """
     choices = [Choice(name=str(arg), value=arg) for arg in args]
 
-    async def autocomplete(_, __) -> List[Choice[str]]:  # noqa ANN001
+    async def autocomplete(_, __) -> List[Choice[_T]]:  # noqa ANN001
         return choices
 
     return autocomplete
