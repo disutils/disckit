cbc280155073291160ca9a8b3f60e31b88f6445f
Jiggly Balls <jigglyballs9000@gmail.com>
2025-05-20 22:37:59 +0530

    Added start functionality & formatting

--- src/disckit/paginator.py
+++ src/disckit/paginator.py
@@ -9,10 +9,9 @@ from discord.ui.view import View
 from disckit.config import UtilConfig
 
 if TYPE_CHECKING:
-    from typing import Sequence
+    from typing import Any, Sequence
 
-    from discord import Embed, Interaction
-    from discord import Button 
+    from discord import Button, Embed, Interaction
 
 
 class Paginator(View):
@@ -21,7 +20,6 @@ class Paginator(View):
         interaction: Interaction,
         pages: Sequence[Embed | str],
         current_page: int = 0,
-        per_page: int = 1,
         new_view: None | View = None,
         timeout: None | float = 180.0,
         home_button: bool = True,
@@ -29,42 +27,69 @@ class Paginator(View):
         ephemeral: bool = False,
     ) -> None:
         super().__init__(timeout=timeout)
+
+        if current_page > len(pages) - 1:
+            raise ValueError(
+                f"Expected an integer of range [0, {len(pages - 1)}]. Instead got {current_page}."
+            )
+
         self.interaction = interaction
         self.pages = pages
         self.current_page = current_page
-        self.per_page = per_page
         self.new_view = new_view
         self.timeout = timeout
         self.home_button = home_button
         self.home_embed = home_embed
         self.ephemeral = ephemeral
 
-    async def start(self) -> None:
-        self.interaction.message
+    def send_kwargs(self, page_element: Embed | str) -> dict[str, Any]:
+        payload = {"view": self}
+        if isinstance(page_element, str):
+            payload["content"] = page_element
+        else:
+            payload["embed"] = page_element
+        return payload
 
-    @discord.ui.button(emoji=UtilConfig.PAGINATOR_FIRST_PAGE, style=ButtonStyle.gray)
+    async def start(self) -> None:
+        element: Embed | str = self.pages[self.current_page]
+        kwargs = self.send_kwargs(element)
+
+        self.children[2].label = f"{self.current_page + 1} / {len(self.pages)}"
+
+        if self.interaction.response.is_done():
+            await self.interaction.followup.send(**kwargs)
+        else:
+            await self.interaction.response.send_message(**kwargs)
+
+    @discord.ui.button(
+        emoji=UtilConfig.PAGINATOR_FIRST_PAGE, style=ButtonStyle.gray
+    )
     async def first_page_callback(
         self, interaction: Interaction, button: Button
     ) -> None: ...
 
-    @discord.ui.button(emoji=UtilConfig.PAGINATOR_PREVIOUS_PAGE, style=ButtonStyle.gray)
+    @discord.ui.button(
+        emoji=UtilConfig.PAGINATOR_PREVIOUS_PAGE, style=ButtonStyle.gray
+    )
     async def previous_page_callback(
         self, interaction: Interaction, button: Button
     ) -> None: ...
 
-    @discord.ui.button(disabled=True, style=ButtonStyle.gray)
+    @discord.ui.button(label="0/5", disabled=True, style=ButtonStyle.gray)
     async def number_page_callback(
-        self, interaction: Interaction, button: Button 
+        self, interaction: Interaction, button: Button
     ) -> None: ...
 
-    @discord.ui.button(emoji=UtilConfig.PAGINATOR_NEXT_PAGE, style=ButtonStyle.gray)
+    @discord.ui.button(
+        emoji=UtilConfig.PAGINATOR_NEXT_PAGE, style=ButtonStyle.gray
+    )
     async def next_page_callback(
         self, interaction: Interaction, button: Button
     ) -> None: ...
 
-    @discord.ui.button(emoji=UtilConfig.PAGINATOR_LAST_PAGE, style=ButtonStyle.gray)
+    @discord.ui.button(
+        emoji=UtilConfig.PAGINATOR_LAST_PAGE, style=ButtonStyle.gray
+    )
     async def last_page_callback(
         self, interaction: Interaction, button: Button
     ) -> None: ...
-
-
