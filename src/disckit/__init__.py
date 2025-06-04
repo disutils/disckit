f06109702008aea66ac204a63eca666d9be9444a
Jiggly Balls <jigglyballs9000@gmail.com>
2025-06-04 17:43:24 +0530

    Version bump

--- src/disckit/__init__.py
+++ src/disckit/__init__.py
@@ -8,7 +8,7 @@ A utility package made for the disutils bots.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __title__ = "disckit"
 __author__ = "Jiggly Balls"
 __license__ = "MIT"
